4ef963d7429e14e403ce08226656b974d06398a6
The third meow <jsheaandcats@gmail.com>
2018-12-11 15:41:01 +1300

    build basic setup/destroy and game-loop functions

--- main.go
+++ main.go
@@ -5,34 +5,79 @@ import (
 	"github.com/veandco/go-sdl2/sdl"
 )
 
+const (
+	SCREEN_STARTPOINT_X = sdl.WINDOWPOS_UNDEFINED
+	SCREEN_STARTPOINT_Y = sdl.WINDOWPOS_UNDEFINED
+	SCREEN_HEIGHT       = 720
+	SCREEN_WIDTH        = 720
+)
+
+//renderer and window is global
+var renderer *sdl.Renderer
+var window *sdl.Window
+
+//checks and handles errors
 func errCheck(err error) {
 	if err != nil {
 		fmt.Println(err)
 	}
 }
 
-func main() {
+func updateAll() {
+	return
+}
+
+//draws everything
+func drawAll() {
+	//draw background
+	renderer.SetDrawColor(0, 5, 0, 30)
+	renderer.Clear()
+
+	//present changes
+	renderer.Present()
+}
+
+//iniyializes everything
+func initializeAll() {
+	//initialize sdl
 	err := sdl.Init(sdl.INIT_EVERYTHING)
 	errCheck(err)
 
-	window, err := sdl.CreateWindow(
+	//create window
+	window, err = sdl.CreateWindow(
 		"A Game I Guess",
-		400,
-		400,
-		500,
-		500,
+		SCREEN_STARTPOINT_X,
+		SCREEN_STARTPOINT_Y,
+		SCREEN_WIDTH,
+		SCREEN_HEIGHT,
 		sdl.WINDOW_OPENGL)
 	errCheck(err)
-	defer window.Destroy()
 
-	renderer, err := sdl.CreateRenderer(window, -1, sdl.RENDERER_ACCELERATED)
+	//create renderer
+	renderer, err = sdl.CreateRenderer(window, -1, sdl.RENDERER_ACCELERATED)
 	errCheck(err)
-	defer renderer.Destroy()
+}
 
+func destroyAll() {
+	window.Destroy()
+	renderer.Destroy()
+}
+
+func main() {
+	initializeAll()
+	defer destroyAll()
+
+	//game loop
 	for {
-		renderer.SetDrawColor(255, 255, 255, 255)
-		renderer.Clear()
-		renderer.Present()
+		updateAll()
+		drawAll()
+		for event := sdl.PollEvent(); event != nil; event = sdl.PollEvent() {
+			switch event.(type) {
+			case *sdl.QuitEvent:
+				//exit program
+				return
+			}
+		}
 	}
 
 }
